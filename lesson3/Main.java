9b49f6ae9b780972fd7ef68429396772a2212b8e
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-04 19:40:24 +0200

    Add files via upload

--- lesson3/Main.java
+++ lesson3/Main.java
@@ -1,29 +1,18 @@
 package com.antongorokh.lesson2task2;
 
+import com.antongorokh.lesson2task2.Cat;
+
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Util.getDistance(1, 2, 5, 13));
     }
 
-    public static class ConsoleReader {
-        static Scanner sc = new Scanner(System.in);
-
-        public static String readString() {
-            return sc.nextLine();
-        }
-
-        public static int readInt() {
-            return sc.nextInt();
-        }
-
-        public static double readDouble() {
-            return sc.nextDouble();
-        }
-
-        public static void readLn() {
-            sc.nextLine();
+    public static class Util {
+        public static double getDistance(double x1, double y1, double x2, double y2) {
+            return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); //according to site bitly.su/6Scs
         }
     }
 }
