d618b65b2cd43a409897aa491b7a0f2845c02001
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-04 18:56:59 +0200

    Add files via upload

---
+++ lesson3/Main.java
@@ -0,0 +1,17 @@
+package com.antongorokh.lesson2task2;
+
+import com.antongorokh.lesson2task2.Cat;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Util.getDistance(1, 2, 5, 13));
+    }
+    public static class Util {
+        public static double getDistance(double x1, double y1, double x2, double y2) {
+            return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); //according to site bitly.su/6Scs
+        }
+    }
+}
+
