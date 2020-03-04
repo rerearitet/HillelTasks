748b71ec74b6bfe89d07e4fe1e49bcb042175073
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-04 19:36:22 +0200

    Add files via upload

---
+++ lesson3/Console.java
@@ -0,0 +1,30 @@
+package com.antongorokh.lesson2task2;
+
+import java.util.Scanner;
+
+public class Console {
+    public static void main(String[] args) {
+
+    }
+
+    public static class ConsoleReader {
+        static Scanner sc = new Scanner(System.in);
+
+        public static String readString() {
+            return sc.nextLine();
+        }
+
+        public static int readInt() {
+            return sc.nextInt();
+        }
+
+        public static double readDouble() {
+            return sc.nextDouble();
+        }
+
+        public static void readLn() {
+            sc.nextLine();
+        }
+    }
+}
+
