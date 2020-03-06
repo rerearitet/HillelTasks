91472e1da66f214d458687706eea8874ca891a44
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-06 16:59:02 +0200

    Add files via upload

--- lesson3/Person.java
+++ lesson3/Person.java
@@ -1,24 +1,25 @@
 package com.antongorokh.lesson2task2;
 
 public class Person {
-    static String name;
-    static Account ACCOUNT;
+    String name;
+    Account account;
 
-    public static void main(String[] args) {
-        name = "Boomer";
-        ACCOUNT = new Account("SuperPyperNagibatorLbv99");
-        ACCOUNT.displayAccount();
-    }
-
-    static class Account {
+    public class Account {
         String password;
+
+        private Account() { //coz who needs account without password?
+        }
 
         public Account(String password) {
             this.password = password;
         }
 
         public void displayAccount() {
-            if (name != null && password != null) System.out.println("Person: " + name + ", " + name + "'s password is: " + password);
+            if (name == null) {
+                System.out.println("Person's name: unknown, person's password: \"" + password + "\".");
+            } else {
+                System.out.println("Person's name: " + name + ", " + name + "'s password: \"" + password + "\".");
+            }
         }
     }
 }
