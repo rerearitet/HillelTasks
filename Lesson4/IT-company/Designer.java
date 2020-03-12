41731965b5a7aaaa75c8a560c68860d859ad7b95
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-12 23:07:16 +0200

    Add files via upload

---
+++ Lesson4/IT-company/Designer.java
@@ -0,0 +1,7 @@
+package com.antongorokh.lesson4.ITcompany;
+
+public class Designer extends Employee {
+    public Designer(int workHours, String... tasks) {
+        super(workHours, tasks);
+    }
+}
---
+++ Lesson4/IT-company/Employee.java
@@ -0,0 +1,21 @@
+package com.antongorokh.lesson4.ITcompany;
+
+import java.util.ArrayList;
+
+public class Employee {
+    int workHours;
+    ArrayList<String> tasksList = new ArrayList<String>();
+
+    public Employee (int workHours, String... tasks) {
+        this.workHours = workHours;
+        for (String task : tasks) tasksList.add(task);
+    }
+
+    @Override
+    public String toString() {
+        String allTasks = "";
+        for (int i = 0; i < tasksList.size() - 1; i++) allTasks += tasksList.get(i) + ", ";
+        allTasks += tasksList.get(tasksList.size() - 1) + ".";
+        return "Work hours: " + workHours + ", Tasks: " + allTasks;
+    }
+}
