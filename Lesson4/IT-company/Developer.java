4629ee586cd5b98be4f7247eb42964807f517edc
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-15 12:31:13 +0200

    Delete Developer.java

--- Lesson4/IT-company/Developer.java
+++
@@ -1,7 +0,0 @@
-package com.antongorokh.lesson4.ITcompany;
-
-public class Developer extends Employee {
-    public Developer(int workHours, String... tasks) {
-        super(workHours, tasks);
-    }
-}
