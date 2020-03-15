0fc9f4a7b4bccabdbbe3b0308f014380660025b8
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-15 12:36:51 +0200

    Delete QualityAssurance.java

--- Lesson4/IT-company/QualityAssurance.java
+++
@@ -1,7 +0,0 @@
-package com.antongorokh.lesson4.ITcompany;
-
-public class QualityAssurance extends Employee {
-    public QualityAssurance(int workHours, String... tasks) {
-        super(workHours, tasks);
-    }
-}
