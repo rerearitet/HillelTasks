56f8a0121c49b3354ccdc2ab8eb00aeb6f358294
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-15 12:52:21 +0200

    Add files via upload

---
+++ Lesson4/IT-company/Main.java
@@ -0,0 +1,25 @@
+package com.antongorokh.lesson4.ITcompany;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Employee designer = new Employee(8, "make new design", "provide new design to manager");
+        Employee developer = new Employee(8, "take task from manager", "fix bugs");
+        Employee manager = new Employee(8, "test new program part", "report developer about bugs");
+        Employee qualityAssurance = new Employee(8, "check project requirements", "call to customer", "review new design");
+
+        try {
+            for (String arg : args) {
+                if (arg.equals("Designer")) {
+                    System.out.println(designer.toString());
+                } else if (arg.equals("Developer")) {
+                    System.out.println(developer.toString());
+                } else if (arg.equals("Manager")) {
+                    System.out.println(manager.toString());
+                } else if (arg.equals("QualityAssurance")) {
+                    System.out.println(qualityAssurance.toString());
+                }
+            }
+        } catch (Exception e) { }
+    }
+}
