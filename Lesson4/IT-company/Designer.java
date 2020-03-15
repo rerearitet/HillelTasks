493d11387657e520b76c75d00644f090f64ddd86
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-15 12:31:06 +0200

    Delete Designer.java

--- Lesson4/IT-company/Designer.java
+++
@@ -1,7 +0,0 @@
-package com.antongorokh.lesson4.ITcompany;
-
-public class Designer extends Employee {
-    public Designer(int workHours, String... tasks) {
-        super(workHours, tasks);
-    }
-}
