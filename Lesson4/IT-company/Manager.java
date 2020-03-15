82a2ea3d67fcb4c181d1310ef8e343e45ab0acc0
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-03-15 12:36:44 +0200

    Delete Manager.java

--- Lesson4/IT-company/Manager.java
+++
@@ -1,7 +0,0 @@
-package com.antongorokh.lesson4.ITcompany;
-
-public class Manager extends Employee {
-    public Manager(int workHours, String... tasks) {
-        super(workHours, tasks);
-    }
-}
