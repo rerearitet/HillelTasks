7544a68bdeefd4e2f8554c22c07d5aac2049519a
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-04-25 13:28:41 +0300

    Add files via upload

--- Task27/Counter.java
+++ Task27/Counter.java
@@ -1,18 +1,24 @@
 package task27;
 
+import java.util.ArrayList;
+
 public class Counter {
     static int COUNTER = 0;
 
     public static void main(String[] args) throws InterruptedException {
+        ArrayList<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             Thread thread = new MyThread(String.valueOf(i));
             thread.start();
-            thread.join();
+            threads.add(thread);
+        }
+        for (int i = 0; i < threads.size(); i++) {
+            threads.get(i).join();
         }
         System.out.println(COUNTER);
     }
 
-    public static void nextCounter() {
+    public synchronized static void nextCounter() {
         COUNTER++;
     }
 }
