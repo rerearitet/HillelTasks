38da6740bd0749be842d4d1203f17abd37f22a6d
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-05-15 19:49:58 +0300

    Add files via upload

---
+++ Task35/Main.java
@@ -0,0 +1,18 @@
+package task35;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ATM atm = new ATM(" ");
+        while (true) {
+            String input = sc.nextLine();
+            if (!input.equals("")) atm.input(input);
+            else {
+                System.out.println("ATM stopped.");
+                break;
+            }
+        }
+    }
+}
